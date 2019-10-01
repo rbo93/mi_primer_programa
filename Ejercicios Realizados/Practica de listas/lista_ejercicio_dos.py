82377da0d978a53e317940ddb1561519bdabae48
rjbo9 <rbo.93@hotmail.com>
2019-10-01 20:51:19 -0300

    Listas: Ejercicio 2 - Multiplicacion de integers de una lista

---
+++ Ejercicios Realizados/Practica de listas/lista_ejercicio_dos.py
@@ -0,0 +1,14 @@
+#Ejercicio 2
+#Dada una lista de numeros, devolver el resultado de la multiplicacion de todos los números.
+
+
+lista_numeros = [2, 5, 10, 8]
+resultado = 1
+
+for dato in lista_numeros:
+
+    resultado = resultado * dato
+
+print(resultado)
+
+
