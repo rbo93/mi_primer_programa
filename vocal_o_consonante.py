6cf1c5cc377fa6fb7d4b8682af3054b94ece0322
rjbo9 <rbo.93@hotmail.com>
2019-10-01 01:11:25 -0300

    Vocal o consonante (el espacio suma a consonante)

---
+++ vocal_o_consonante.py
@@ -0,0 +1,12 @@
+frase_del_usuario = input("Introduce una frase: ")
+vocales = ["a", "e", "i", "o", "u"]
+n_vocales = 0
+n_consonantes = 0
+for letra in frase_del_usuario:
+    if letra in vocales:
+        n_vocales += 1
+    else:
+        n_consonantes += 1
+
+print("Las vocales son {}: ".format(n_vocales))
+print("Las consonantes son {}: ".format(n_consonantes))
