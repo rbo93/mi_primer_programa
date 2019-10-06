5d73ae44cae7c9812ffc9d95c2fbc86abb9413e9
rjbo9 <rbo.93@hotmail.com>
2019-10-06 04:10:07 -0300

    Diccionarios - Ejercicio 1 Nombre y año de nacimiento

---
+++ Ejercicios Realizados/Diccionarios/diccionarios_ejercicio_uno.py
@@ -0,0 +1,25 @@
+"""
+Ejercicio 1
+
+Crea un programa que sea capaz de guardar los nombres de tus amigos y sus años de nacimiento.
+
+"""
+
+salida = False
+agenda = dict()
+while not salida:
+    accion = input("¿Que quieres hacer? Añadir fecha[A] / Consultar numero[C]] / Salir[S]" )
+    # Añadir fecha
+    if accion == "A":
+        print("Vamos a añadir un año de nacimiento")
+        nombre = input("Nombre: ")
+        year = input("Años de nacimiento: ")
+        agenda[nombre] = year
+    # Consultar numero
+    elif accion == "C":
+        print("Consultar año de nacimiento: ")
+        nombre = input("De quien quieres saber el año de nacimiento:  ")
+        print(agenda[nombre])
+
+    elif accion == "S":
+        salida = True
