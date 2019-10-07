feb5158c7931158d8e03303fd1fc18f1dced3870
rjbo9 <rbo.93@hotmail.com>
2019-10-07 05:18:20 -0300

    Ejercicio 3 - Diccionarios suma la cantidad de palabras de un string

---
+++ Ejercicios Realizados/Diccionarios/diccionarios_ejercicio_tres.py
@@ -0,0 +1,32 @@
+
+def busca_palabras(palabras, diccionario):
+    if "Hola" == palabras:
+        diccionario["Hola"] += 1
+    elif "como" == palabras:
+        diccionario["como"] += 1
+    elif "estas" == palabras:
+        diccionario["estas"] += 1
+    elif "amigo" == palabras:
+        diccionario["amigo"] += 1
+    return
+
+
+string = "Hola Hola como estas amigo amigo amigo"
+apariciones_palabras = {"Hola": 0, "como": 0, "estas": 0, "amigo": 0}
+lista_de_palabras = []
+palabra = ""
+i = 0
+
+for index in string:
+    if index != " ":
+        palabra += index
+    else:
+        lista_de_palabras.append(palabra)
+        palabra = ""
+f = len(lista_de_palabras)
+while i <= f - 1:
+    busca_palabras(lista_de_palabras[i], apariciones_palabras)
+    i += 1
+
+print(lista_de_palabras)
+print(apariciones_palabras)
